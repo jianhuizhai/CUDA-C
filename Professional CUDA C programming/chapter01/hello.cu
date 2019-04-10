334580dc7ed52baeeb083fec3eaa33df18dfcc9c
jianhuizhai <jianhuizhai89@gmail.com>
2019-04-10 22:28:54 +0200

    Add files via upload

---
+++ Professional CUDA C programming/chapter01/hello.cu
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+__global__ void helloFromGPU(void)
+{
+    if (threadIdx.x == 5)
+    {
+        printf("Hello World from GPU thread %d! \n", threadIdx.x);
+    }
+    
+}
+
+int main(void)
+{
+    printf("Hello World from CPU ! \n");
+    helloFromGPU <<<1,10>>>();
+    cudaDeviceReset();
+    //cudaDeviceSynchronize();
+    return 0;
+}
